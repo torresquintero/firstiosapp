7c88b07ef1b8c38a18362ce625f28e3f480f46eb
Alexandra Torresquintero <alexandratorresquintero@SCFIL31.local>
2017-01-03 17:30:35 -0500

    first ios app

--- Getting Started/ViewController.swift
+++ Getting Started/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clickCountLabel: UILabel!
+    
+    var clickCount: Int = 0 {
+        didSet {
+            clickCountLabel.text = "\(clickCount)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        clickCountLabel.text = "0"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func clickButton(_ sender: Any) {
+        clickCount += 1
     }
-
-
 }
 
